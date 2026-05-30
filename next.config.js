a4a67eea3b0a07ec32ccab7a07af55d7ec2c5f1d
fatnarman-debug <fatnarman@gmail.com>
2026-05-30 23:43:29 +0200

    fix: update CSP to allow YouTube and Vimeo iframe embeds

--- next.config.js
+++ next.config.js
@@ -35,7 +35,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https:; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https:; frame-src 'self' https://js.stripe.com;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https:; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https:; frame-src 'self' https://js.stripe.com https://www.youtube.com https://youtube.com https://player.vimeo.com;",
           },
         ],
       },
